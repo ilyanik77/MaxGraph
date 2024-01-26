261b029e6f82dc3c91188c0557cd3a59f891f21d
ilya <ilyakhlamov77@gmail.com>
2024-01-26 22:12:48 +0300

    add 26.01.2024

---
+++ js/videosSlider.js
@@ -0,0 +1,14 @@
+// const slider = document.querySelector(".swiper-container");
+
+// let swiperSlider = new Swiper(slider, {
+  
+//   centeredSlides: true,
+//   //slidesPerView: "auto",
+//   loop: true,
+//   spaceBetween: 105,
+// });
+new Swiper(".swiper-container", {
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 105,
+});
